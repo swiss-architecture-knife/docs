a1993cb0d01ab8983f4cf61e741dd07d1baa5a6c
Christopher Klein <ckl@dreitier.com>
2025-02-08 12:05:00 +0100

    fix: deployment project
    Signed-off-by: Christopher Klein <ckl@dreitier.com>

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'swiss-architecture-knife', // Usually your GitHub org/user name.
-  projectName: 'swark', // Usually your repo name.
+  projectName: 'swiss-architecture-knife.github.io', // Usually your repo name.
   trailingSlash: false,
 
   onBrokenLinks: 'throw',
